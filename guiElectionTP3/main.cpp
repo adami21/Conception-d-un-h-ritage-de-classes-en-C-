f8e0235c4659aa6d2a993a8591aea6dfa1099c0a
adami21 <adami2@ulaval.ca>
2024-12-06 14:01:23 -0500

    creation fichier qt

---
+++ guiElectionTP3/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/qtFiles/main.cc to edit this template
+ */
+
+/*
+ * File:   main.cpp
+ * Author: etudiant
+ *
+ * Created on 6 décembre 2024, 08 h 44
+ */
+
+#include <QApplication>
+
+int
+main (int argc, char *argv[])
+{
+  // initialize resources, if needed
+  // Q_INIT_RESOURCE(resfile);
+
+  QApplication app (argc, argv);
+
+  // create and show your widgets here
+
+  return app.exec ();
+}
